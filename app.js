477f62a09ab6e4b2ad28a0dcc6f9bcf66bc7e836
Casey Woolfolk <ccwoolfolk@gmail.com>
2016-10-09 20:17:16 +0000

    Set up basic Pug structure

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-// collection = albums
+"use strict";
+
+const express = require("express");
+const pug = require("pug");
+const model = require("./models/event.js");
+
+const fixtures = require('./test/fixtures/model-albums').data;
+
+const app = express();
+const DB = require("./db.js");
+
+// Add middleware
+app.set("view engine", "pug");
+app.set("json spaces", 3);
+
+app.get("/", (req, res) => model.getEvents(1, (err, events) => {
+    if (err) 
+        console.error(err);
+    
+    res.render("events", {events: events});
+    
+}));
+
+
+DB.connect(DB.MODE_TEST, () => DB.fixtures(fixtures, () => {
+    app.listen(8080, () => console.log("listening on 8080") );
+}));
+    
