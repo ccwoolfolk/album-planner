6b0a2d7aabd5a0d2b80236f8ae3bec6d1ac85662
Casey Woolfolk <ccwoolfolk@gmail.com>
2016-10-10 21:57:25 +0000

    Remove trailing / from URLs

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ app.set("view engine", "pug");
 app.set("json spaces", 3);
 
 app.use(express.static("public"));
+app.use(function(req, res, next) {
+   if(req.url.substr(-1) == '/' && req.url.length > 1)
+       res.redirect(301, req.url.slice(0, -1));
+   else
+       next();
+});
 
 /* Show the user's events when provided a user ID */
 app.get("/:userId", (req, res) => model.getEvents(req.params.userId, (err, events) => {
